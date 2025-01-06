ec6930094736b931ef4f1d37f06c0c156872e0e1
shajahan57 <shajahaniqbal57@gmail.com>
2025-01-06 09:54:20 +0530

    dynamic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { UserProfile } from "./components/UserProfile"
+import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
-    const callMe = () => {
-        console.log("hello");
-    };
-    
-    return (
-        <UserProfile 
-            username="Bob"
-            age={20}
-            isLoggedIn={false}
-            favouriteFoods={
-                [
-                    {
-                        name: "Pizza",
-                        id: 1
-                    },
-                    {
-                        name: "Burger",
-                        id: 2
-                    },                    
-                    {
-                        name: "Shawarma",
-                        id: 3
-                    }
-                ]
-            }
-            callMe={callMe}
-        />
-        )
+    const mockUsers = [
+        {
+            id: 1,
+            username: "anson",
+            email: "anson@ansonthedev.com"
+        },
+        {
+            id: 2,
+            username: "michael",
+            email: "michael@ansonthedev.com"
+        }
+    ]
+
+    return <div>
+        {mockUsers.map(
+            (user) => {
+                return <UserDetails key={user.id} user={user}/>
+            })}
+        </div>;
+        
+
 }
