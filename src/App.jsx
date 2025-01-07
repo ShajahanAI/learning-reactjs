e90e553849a71e59a344aff2970f42c0f00e6e12
shajahan57 <shajahaniqbal57@gmail.com>
2025-01-07 06:31:08 +0530

    Conditional Rendering - switch case

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,37 @@
 export default function App() {
     // NOT_VERIFIED | VERIFIED | ACCOUNT_DISABLED
-    const USER_STATUS = null;
-
-    if (USER_STATUS === "NOT_VERIFIED") {
-        return (
-            <div>
-                <span>
-                    You are not verified. Please verify your Email or Mobile Number.
-                </span>
-            </div>
-        );
+    const USER_STATUS = "VERIFIED";
+    
+    switch (USER_STATUS) {
+        case "NOT_VERIFIED":
+            return (
+                <div>
+                    <span>
+                        You are not verified. Please verify your Email or Mobile Number.
+                    </span>
+                </div>
+            );
+        case "VERIFIED":
+            return (
+                <div>
+                    <span>
+                        You are verified. Congrates! Click here to access your dashboard.
+                    </span>
+                </div>
+            )
+        case 'ACCOUNT_DISABLED':
+            return (
+                <div>
+                    <span>Your account is disabled</span>
+                </div>
+            )
+        default:
+            return (
+                <div>
+                    <span>
+                        Please contact a system admin.
+                    </span>
+                </div>
+            )
     }
-
-    else if (USER_STATUS === "VERIFIED") {
-        return (
-            <div>
-                <span>
-                    You are verified. Congrates! Click here to access your dashboard.
-                </span>
-            </div>
-        )
-    }
-    else if (USER_STATUS == 'ACCOUNT_DISABLED') {
-        return (
-            <div>
-                <span>Your account is disabled</span>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div>
-                <span>
-                    Please contact a system admin.
-                </span>
-            </div>
-        )
-    }
-
 }
