8e6f2355eec8d9d5470b9c27065adb9b47c91119
shajahan57 <shajahaniqbal57@gmail.com>
2025-01-26 07:42:18 +0530

    submit form, created post functionality for blogs

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,73 @@ import { func } from "prop-types";
 import { useState, useEffect, useDeferredValue } from "react";
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  const [sync, setSync] = useState(false);
-
-  useEffect(() => {
-    console.log("Rendering...");
-    document.title = "React Tutorial " + counter;
-  }, [sync]);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    async function fetchUsers() {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
-          {
-            method: "GET",
-            signal: controller.signal
-          }
-        );
-
-        const json = await response.json();
-        console.log(json);
-        console.log(controller.signal);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    fetchUsers();
-    return () => {
-      controller.abort();
-      console.log(controller.signal);
-    }
+  const [blogPostData, setBlogPostData] = useState({
+    title: "",
+    body: "",
   });
 
   return (
     <div>
-      <div>You clicked this button {counter} times</div>
-      <br />
-      <button
-        onClick={() => setCounter((currentCounter) => currentCounter + 1)}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (blogPostData.title && blogPostData.body) {
+            fetch("https://jsonplaceholder.typicode.com/posts", {
+              method: "POST",
+              body: JSON.stringify({
+                userId: 1,
+                id: 1,
+                title: blogPostData.title,
+                body: blogPostData.body,
+              }),
+              headers: {
+                "Content-type": "application/json; charset=UTF-8",
+              },
+            })
+              .then((response) => response.json())
+              .then((data) => console.log("Success", data))
+              .catch((err) => console.log("Err", err));
+          }
+        }}
       >
-        Click Me
-      </button>
-      <button onClick={() => setSync((currentSync) => !currentSync)}>
-        Sync
-      </button>
+        <label htmlFor="title" style={{ paddingRight: 5 }}>
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          value={blogPostData.title}
+          onChange={(e) => {
+            setBlogPostData((currentBlogPostData) => {
+              return {
+                ...currentBlogPostData,
+                title: e.target.value,
+              };
+            });
+          }}
+        />
+        <br />
+        <br />
+        <label htmlFor="body" style={{ paddingRight: 5 }}>
+          Body
+        </label>
+        <input
+          type="text"
+          id="body"
+          value={blogPostData.body}
+          onChange={(e) => {
+            setBlogPostData((currentBlogPostData) => {
+              return {
+                ...currentBlogPostData,
+                body: e.target.value,
+              };
+            });
+          }}
+        />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
